cb4010bc555049d97a47013c735021d235c7c3dd
Adrian Cerdeira <adrian.cerdeira@ongoing.ch>
2023-03-31 12:00:57 +0200

    feat: implement navbar

---
+++ pages/navbar.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" href="/">Navbar</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/">Link</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </Link>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <Link className="dropdown-item" href="/">Action</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <Link className="btn btn-primary" href="/login">Login</Link>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
